33c6671f846975f0e933c1f224a838e593c93bc0
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-09-18 14:15:48 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows access via your IP
-    port: 5173  // Or any port you want
+  build: {
+    outDir: 'dist' // Default output directory for Vite
   }
-})
+});
