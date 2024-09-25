f1869eb1d8f53bb18fadd942f28b0df87bb4356c
abhayinfinix <shubhamdasinfinix@gmail.com>
2024-09-25 22:39:21 +0530

    24/09/24/pincode-done

--- public/constant.js
+++ public/constant.js
@@ -1,5 +1,5 @@
 
 // src/public/constant.js
-const REACT_APP_API_URL = 'http://localhost:12000';
-// const REACT_APP_API_URL = 'https://bharat-lbackend.vercel.app';
+// const REACT_APP_API_URL = 'http://localhost:12000';
+const REACT_APP_API_URL = 'https://bharat-lbackend.vercel.app';
 export default REACT_APP_API_URL;
