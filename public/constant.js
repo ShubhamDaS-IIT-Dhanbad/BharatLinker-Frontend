d5e508ad33008034cef8930f7d3102386c866c8b
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2024-09-30 00:22:33 +0530

    Your commit message here

--- public/constant.js
+++ public/constant.js
@@ -2,6 +2,7 @@
 // src/public/constant.js
 // const REACT_APP_API_URL = 'http://localhost:12000';
 const REACT_APP_API_URL = 'https://bharat-lbackend.vercel.app';
-// export const RETAILER_SERVER='http://localhost:3002';
+// export const RETAILER_SERVER='http://localhost:3002;
+//HELOW
 export const RETAILER_SERVER='https://bharat-linker-retailer.vercel.app'
 export default REACT_APP_API_URL;
