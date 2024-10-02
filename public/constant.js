f5b70cecd4c8b4594cd529bc6419c8d2853941df
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2024-10-02 23:37:58 +0530

    push

--- public/constant.js
+++ public/constant.js
@@ -2,8 +2,8 @@
 // src/public/constant.js
 // const REACT_APP_API_URL = 'http://localhost:12000';
 const REACT_APP_API_URL = 'https://bharat-lbackend.vercel.app'
-export const RETAILER_SERVER='http://localhost:3002'
-// export const RETAILER_SERVER='https://bharat-linker-retailer.vercel.app'
+// export const RETAILER_SERVER='http://localhost:3002'
+export const RETAILER_SERVER='https://bharat-linker-retailer.vercel.app'
 export const RETAILER_PRODUCT_SERVER='https://bharat-linker-products.vercel.app'
 // export const RETAILER_PRODUCT_SERVER='http://localhost:3001'
 export default REACT_APP_API_URL;
